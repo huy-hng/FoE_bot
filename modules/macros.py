e8a7e10ef95f00942734c735a4fa538e6c20d34b
Huy Hoang <huyhoang0496@gmail.com>
2019-03-12 00:56:07 +0100

    macros

--- modules/macros.py
+++ modules/macros.py
@@ -1,38 +1,48 @@
-import time
 import os
 import pickle
+import time
 import winsound
+from pprint import PrettyPrinter
 
 import consolemenu as cm
 import consolemenu.items as cm_items
 import pyautogui
-import win32api, win32con
+import win32con
 
+import __init__
+import win32api
+from HuysStudio.python.templates.huys_logging import Logging
 from vk_codes import VK_CODE, BINDABLE_keys
 
 
+
 class Preset:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        # self.log_preset = Logging('Macros.Preset', level='debug', filter_str='', create_file=True)
 
         self.macros = {}
         for key in BINDABLE_keys:
             self.macros[key] = None
 
 
-    def show_preset_menu(self):
-        menu = cm.ConsoleMenu(f'{self.name}')
 
-        macro = cm_items.MenuItem('Create new Macro')
-        delete = cm_items.MenuItem('Delete Preset')
-        back = cm_items.MenuItem('Back to Menu')
+    def test(self):
+        # logger = self.log_preset.get_logger('test')
+    
+        try:
+            pass
+        except Exception as e:
+            # logger.exception(e)
+            pass
 
-        menu.append_item(macro)
-        menu.append_item(delete)
-        menu.append_item(back)
+    def back(self):
+        # logger = self.log_preset.get_logger('back')
 
-        menu.show()
-
+        try:
+            pass
+        except Exception as e:
+            # logger.exception(e)
+            pass
 
 
     def create_new_macro(self):
@@ -57,6 +67,7 @@ class Preset:
 
                 if self.key_pressed('esc'):
                     return
+
 
 
     def main(self):
@@ -90,6 +101,7 @@ class Preset:
             time.sleep(0.01)
 
 
+
     #region helpers
     @staticmethod
     def key_pressed(key):
@@ -116,33 +128,143 @@ class Preset:
 
 
 
+class MainMenu:
+    def __init__(self):
+        self.log_main_menu = Logging('Macros.Menu', level='debug', filter_str='', create_file=True)
 
-def preset_list_menu():
-    if os.path.isfile('data/macros.data'):
-        with open('data/macros.data', 'rb') as f:
-            presets = pickle.load(f)
-    else:
+        self.main_menu = cm.ConsoleMenu('Macros', 'Set mouse positions as macros.')
+        create_preset_menu = cm_items.FunctionItem('Create new Preset', self.create_new_preset)
+        self.main_menu.append_item(create_preset_menu)
+
+        self.load_file()
+
+        self.main_menu.show()
+
+
+    def create_new_preset(self):
+        global presets
+
+        name = input('Preset Name: ')
+
+        preset = Preset()
+        presets[name] = preset
+
+        PresetMenu(self.main_menu, name, preset)
+
+    def load_file(self):
+        global presets
+
+        logger = self.log_main_menu.get_logger('load_file')
+
         presets = {}
+        if os.path.isfile('data/macros.data'):
+            with open('data/macros.data', 'rb') as f:
+                presets = pickle.load(f)
+                logger.debug(presets)
 
-
-    menu = cm.ConsoleMenu('Macros', 'Set mouse positions as macros.')
-
-    create_preset_menu = cm_items.FunctionItem('Create new preset.', create_preset)
-    menu.append_item(create_preset_menu)
-
-    if presets:
-        for key in presets:
-            preset = cm_items.FunctionItem(key, input, presets[key])
-            menu.append_item(preset)
-
-    menu.show()
-
-
-def create_preset(self):
-    
-    pass
+        if presets:
+            for name, preset in presets.items():
+                PresetMenu(self.main_menu, name, preset)
 
 
 
-# main()
-Macros()
+
+
+class PresetMenu:
+    def __init__(self, main_menu, name, preset):
+        self.log_preset_menu = Logging('Macros.Menu', level='debug', filter_str='', create_file=True)
+
+        self.main_menu = main_menu
+        self.name = name
+        self.preset = preset
+
+        create_preset_menu()
+
+
+    def create_preset_menu(self):
+
+        self.preset_menu = cm.ConsoleMenu(f'{self.name}')
+
+        self.submenu_item = cm_items.SubmenuItem(f'{self.name}', self.preset_menu, self.main_menu)
+        self.main_menu.append_item(self.submenu_item)
+
+        macro = cm_items.MenuItem('Create new Macro')
+        line = cm_items.MenuItem('─────────────────────')
+        change_name = cm_items.FunctionItem('Change Preset Name', lambda: self.change_name(self.name))
+        delete = cm_items.FunctionItem('Delete Preset', lambda: self.delete_preset(self.name))
+
+        back = cm_items.FunctionItem('Back', self.preset.back)
+        test = cm_items.FunctionItem('Test', self.preset.test)
+
+        self.preset_menu.append_item(macro)
+        self.preset_menu.append_item(line)
+        self.preset_menu.append_item(change_name)
+        self.preset_menu.append_item(delete)
+
+        # preset_menu.append_item(back)
+        # preset_menu.append_item(test)
+        
+
+
+    #region preset menu functions
+    def delete_preset(self, name):
+        global presets
+        logger = self.log_preset_menu.get_logger('delete_preset')
+
+        try:
+            self.main_menu.remove_item(self.main_menu.current_item)
+            self.main_menu.draw()
+
+            del presets[name]
+
+            write_file()
+
+            # self.preset_menu.exit()
+
+        except Exception as e:
+            logger.exception(e)
+
+
+    def change_name(self, name):
+        global presets
+        logger = self.log_preset_menu.get_logger('change_name')
+
+
+        try:
+            new_name = input('New Name: ')
+
+            # change menu title and main menu name
+            self.preset_menu.title = new_name
+            self.submenu_item.title = new_name
+            
+            logger.debug(f'{self.preset_menu}, {type(self.preset_menu)}')
+            logger.debug(f'{self.submenu_item}, {type(self.submenu_item)}')
+            logger.debug(f'{self.preset_menu.title}, {type(self.preset_menu.title)}')
+
+            self.main_menu.draw()
+            self.preset_menu.draw()
+
+            presets[new_name] = presets[name]
+            del presets[name]
+            logger.debug(presets)
+
+            # self.show_preset_menu()
+
+            write_file()
+        except Exception as e:
+            logger.exception(e)
+    #endregion
+
+
+
+
+presets = {}
+
+def write_file():
+    with open('data/macros.data', 'wb') as f:
+        pickle.dump(presets, f)
+
+MainMenu()
+
+
+
