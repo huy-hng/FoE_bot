f4a6b4f463ba10a400b3ff6fcdf916e5891b1800
Huy Hoang <huyhoang0496@gmail.com>
2019-03-18 21:41:54 +0100

    macros

--- modules/macros.py
+++ modules/macros.py
@@ -2,7 +2,7 @@ import os
 import pickle
 import time
 import winsound
-from pprint import PrettyPrinter
+import concurrent.futures
 
 import consolemenu as cm
 import consolemenu.items as cm_items
@@ -15,41 +15,45 @@ from Sandbox.python.templates.huys_logging import Logging
 from vk_codes import VK_CODE, BINDABLE_keys
 
 
-
 class Preset:
-	def __init__(self):
-		# self.log_preset = Logging('Macros.Preset', level='debug', filter_str='', create_file=True)
+	def __init__(self, name):
+		self.log_preset = Logging('Macros.Preset', level='debug', filter_str='', create_file=True)
+		self.name = name
 
-		self.macros = {}
-		for key in BINDABLE_keys:
-			self.macros[key] = None
-
+	@property
+	def macros(self):
+		global presets
+		return presets[self.name]
 
 	def create_new_macro(self):
-		winsound.Beep(440, 200)
 
-		while True:
-			x, y = pyautogui.position()
-			time.sleep(0.05)
+		logger = self.log_preset.get_logger('create_new_macro')
+		try:
+			time.sleep(0.5)
+			winsound.Beep(440, 200)
 
-			for k, _ in self.macros.items():
-				if self.key_pressed(f'{k}'):
-					self.macros[f'{k}'] = [x, y]
+			while True:
+				x, y = pyautogui.position()
+				time.sleep(0.05)
 
-					return
+				for key, _ in self.macros.items():
+					if self.key_pressed(f'{key}'):
+						logger.debug(f'key: {key}')
+						winsound.Beep(880, 100)
+						self.macros[f'{key}'] = [x, y]
+						write_file()
+						return key, [x, y]
 
-				if self.key_pressed('esc'):
-					return
+					if self.key_pressed('esc'):
+						return None
+		except Exception as e:
+			logger.exception(e)
 
 
-	def main(self):
+	def start(self):
 
-		for key, val in self.macros.items():
-			if val is not None:
-				print(f'{key}: {val[0]}, {val[1]}')
-
-
-		while True:
+		self.start = True
+		while self.start:
 
 			for k, _ in self.macros.items():
 				if self.key_pressed(f'{k}'):
@@ -63,7 +67,6 @@ class Preset:
 						time.sleep(0.1)
 
 
-
 			if self.key_pressed('F4'):
 				self.create_new_macro(self.macros)
 
@@ -71,6 +74,9 @@ class Preset:
 				self.pause()
 
 			time.sleep(0.01)
+
+	def stop(self):
+		self.start = False
 
 
 	#region helpers
@@ -121,10 +127,16 @@ class MainMenu:
 
 		name = input('Preset Name: ')
 
-		preset = Preset()
-		presets[name] = preset
+		preset = Preset(name)
 
-		PresetMenu(self.main_menu, name)
+
+		macros = {}
+		for key in BINDABLE_keys:
+			macros[key] = None
+
+		presets[name] = macros
+
+		PresetMenu(self.main_menu, name, preset)
 
 
 	def load_file(self):
@@ -136,20 +148,23 @@ class MainMenu:
 		if os.path.isfile('data/macros.data'):
 			with open('data/macros.data', 'rb') as f:
 				presets = pickle.load(f)
+				logger.debug(f'presets: {presets}')
 
 		if presets:
 			for name in presets:
 				logger.debug(f'name: {name}')
-				PresetMenu(self.main_menu, name)
+				preset = Preset(name)
+				PresetMenu(self.main_menu, name, preset)
 
 
 class PresetMenu:
-	def __init__(self, main_menu, name):
+	def __init__(self, main_menu, name, preset):
 		self.log_preset_menu = Logging('Macros.PresetMenu', level='debug', filter_str='',
 										print_=False, create_file=True, time=True)
 
 		self.main_menu = main_menu
 		self.name = name
+		self.preset = preset
 
 		self.toggle_start_stop = 'start'
 
@@ -165,17 +180,18 @@ class PresetMenu:
 		self.main_menu.append_item(self.submenu_item)
 
 
-		self.start_stop =	cm_items.FunctionItem('Start', self.start_stop)
-		self.macro =		cm_items.MenuItem('Create new Macro')
-		edit_macros =		cm_items.MenuItem('Edit Macros')
-		line =				cm_items.MenuItem('─────────────────────')
-		change_name =		cm_items.FunctionItem('Change Preset Name', self.change_name)
-		delete =			cm_items.FunctionItem('Delete Preset', self.delete_sub_menus)
+
+		start_stop		= cm_items.FunctionItem('Start'             , self.start_stop            )
+		create_macro	= cm_items.FunctionItem('Create new Macro'  , self.create_macro			 )
+		line			= cm_items.MenuItem    ('─────────────────────'                    )
+		change_name		= cm_items.FunctionItem('Change Preset Name', self.change_name           )
+		delete          = cm_items.FunctionItem('Delete Preset'     , self.submenus_delete_preset)
 
 
-		self.preset_menu.append_item(self.start_stop)
-		self.preset_menu.append_item(self.macro)
-		self.preset_menu.append_item(edit_macros)
+		self.preset_menu.append_item(start_stop)
+		self.preset_menu.append_item(create_macro)
+		self.preset_menu.append_item(line)
+		self.submenu_macro()
 		self.preset_menu.append_item(line)
 		self.preset_menu.append_item(change_name)
 		self.preset_menu.append_item(delete)
@@ -186,12 +202,12 @@ class PresetMenu:
 
 		try:
 			if self.toggle_start_stop == 'start':
-				# presets[self.name].start
+				self.preset.start
 				self.start_stop.text = 'Stop (END)'
 				self.macro.text = 'Create new Macro (F4)'
 				self.toggle_start_stop = 'stop'
 			else:
-				# presets[self.name].stop
+				self.preset.stop
 				self.start_stop.text = 'Start'
 				self.macro.text = 'Create new Macro'
 				self.toggle_start_stop = 'start'
@@ -199,19 +215,68 @@ class PresetMenu:
 		except Exception as e:
 			logger.exception(e)
 
+	def create_macro(self):
+		key, _ = self.preset.create_new_macro()
+		macro = cm_items.MenuItem(key)
+		self.preset_menu.append_item(macro)
+		self.preset_menu.draw()
 
-	def macros_menu(self):
-		self.macros_menu = cm.ConsoleMenu(f'Macros - {self.name} - Macro list')
+	def delete_macro(self, menu):
 
-		for key, coord in presets[self.name].macros.items():
+		menu.current_option = len(menu.items) - 1
+		menu.select()
+		self.preset_menu.show()
+
+
+	#region submenus
+	def submenu_create_macro(self):
+
+		logger = self.log_preset_menu.get_logger('submenu_create_macro')
+		try:
+			create_macro_menu = cm.ConsoleMenu(f'Macros - {self.name} - Create Macro',
+				'Put your mouse over the desired location and press a button on your keyboard to bind it.',
+				show_exit_option=True)
+			# key_item = cm_items.MenuItem('Press a key.')
+			# back = cm_items.MenuItem('Back', should_exit=True)
+			# create_macro_menu.append_item(key_item)
+			# create_macro_menu.append_item(back)
+			result = self.preset.create_new_macro()
+
+			create_macro_menu.show()
+
+
+			# if result is not None:
+			# 	key_item.text = f'{result[0]}: {result[1]}'
+			
+		except Exception as e:
+			logger.exception(e)
+
+
+
+
+
+	def submenu_macro(self):
+
+		for key, coord in self.macros.items():
 			if coord is not None:
-				self.submenu_item = cm_items.SubmenuItem(f'{self.name}', self.preset_menu, self.main_menu)
-				print(f'{key}: {coord[0]}, {coord[1]}')
+				macro_menu = cm.ConsoleMenu(f'Macros - {self.name} - {key}')
+
+				edit_macro = cm_items.FunctionItem('Edit Macro', self.start_stop)
+				delete_macro = cm_items.FunctionItem('Delete Macro', lambda: self.delete_macro(macro_menu))
+
+				macro_menu.append_item(edit_macro)
+				macro_menu.append_item(delete_macro)
+
+				macro_submenu = cm_items.SubmenuItem(f'{key}', macro_menu, self.preset_menu)
+				self.preset_menu.append_item(macro_submenu)
+				self.preset_menu.draw()
+
+		
+
+				# print(f'{key}: {coord[0]}, {coord[1]}')
 
 
-	#region preset menu functions
-
-	def change_name_menu(self):
+	def submenu_change_name(self):
 		logger = self.log_preset_menu.get_logger('change_name_menu')
 		menu = cm.ConsoleMenu('Give it a good name', 
 			'Like 420blazeitfaggot or maybe duckmysickUcuckingfunt', show_exit_option=True)
@@ -221,6 +286,52 @@ class PresetMenu:
 		logger.debug(f'new_name: {new_name}')
 
 
+	def submenus_delete_preset(self):
+
+		delete_very_sad = lambda: self.submenu_choice(':(', [':((', ':((('])
+		delete_ok_sorry = lambda: self.submenu_choice('Too late bitch', [':(', ':(('])
+
+		delete_after_menu = lambda: self.submenu_choice('Bitch y u press in the first place then', 
+												["Ok sorry, you're right. Delete!", '¯\_(ツ)_/¯'],
+												[delete_ok_sorry, delete_very_sad])
+
+		delete_prompt = lambda: self.submenu_choice('Delete?', ['Yes!', 'Wait nO'], 
+											[self.delete_preset, delete_after_menu])
+
+		delete_prompt()
+
+
+	def submenu_choice(self, title, text, function=None):
+
+		logger = self.log_preset_menu.get_logger(f'choice: {title}')
+
+		try:
+			menu = cm.ConsoleMenu(f'{title}', show_exit_option=False)
+
+			for i, text in enumerate(text):
+				if function is not None:
+					if function[i] is not None:
+						choice = cm_items.FunctionItem(f'{text}', function[i], should_exit=True)
+				else:
+					choice = cm_items.MenuItem(f'{text}', should_exit=True)
+
+				menu.append_item(choice)
+
+
+			menu.show()
+			
+		except Exception as e:
+			logger.exception(e)
+	#endregion submenus
+
+
+
+	@property
+	def macros(self):
+		global presets
+		return presets[self.name]
+
+	#region preset menu functions
 	def change_name(self):
 		global presets
 		logger = self.log_preset_menu.get_logger('change_name')
@@ -243,43 +354,6 @@ class PresetMenu:
 			# self.show_preset_menu()
 
 			write_file()
-		except Exception as e:
-			logger.exception(e)
-
-
-	def delete_sub_menus(self):
-
-		delete_very_sad = lambda: self.choice(':(', [':((', ':((('])
-		delete_ok_sorry = lambda: self.choice('Too late bitch', [':(', ':(('])
-
-		delete_after_menu = lambda: self.choice('Bitch y u press in the first place then', 
-												["Ok sorry, you're right. Delete!", '¯\_(ツ)_/¯'],
-												[delete_ok_sorry, delete_very_sad])
-
-		delete_prompt = lambda: self.choice('Delete?', ['Yes!', 'Wait nO'], 
-											[self.delete_preset, delete_after_menu])
-
-		delete_prompt()
-
-
-	def choice(self, title, text, function=None):
-
-		logger = self.log_preset_menu.get_logger(f'choice: {title}')
-
-		try:
-			menu = cm.ConsoleMenu(f'{title}', show_exit_option=False)
-
-			for i, text in enumerate(text):
-				choice = cm_items.MenuItem(f'{text}', should_exit=True)
-				if function is not None:
-					if function[i] is not None:
-						choice = cm_items.FunctionItem(f'{text}', function[i], should_exit=True)
-
-				menu.append_item(choice)
-
-
-			menu.show()
-			
 		except Exception as e:
 			logger.exception(e)
 
@@ -316,6 +390,3 @@ def write_file():
 		pickle.dump(presets, f)
 
 MainMenu()
-
-
-
