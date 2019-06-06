ac270d4d0308bbb99eac249da7bb4c0fd4d2a0af
Huy Hoang <huyhoang0496@gmail.com>
2019-06-06 19:03:02 +0200

    works kinda

--- electron/screenshot.js
+++ electron/screenshot.js
@@ -1,40 +1,61 @@
 // In the renderer process.
 const { desktopCapturer } = require("electron");
 
-desktopCapturer
-  .getSources({ types: ["window", "screen"] })
-  .then(async sources => {
-    for (const source of sources) {
-      console.log(source.name);
+function getScreenshot() {
+  desktopCapturer
+    .getSources({ types: ["window", "screen"] })
+    .then(async sources => {
+      for (const source of sources) {
+        console.log(source.name);
 
-      if (source.name === "FoE Bot") {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia({
-            audio: false,
-            video: {
-              mandatory: {
-                chromeMediaSource: "desktop",
-                chromeMediaSourceId: source.id,
-                minWidth: 1280,
-                maxWidth: 1280,
-                minHeight: 720,
-                maxHeight: 720
+        if (source.name === "FoE Bot") {
+          try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+              audio: false,
+              video: {
+                mandatory: {
+                  chromeMediaSource: "desktop",
+                  chromeMediaSourceId: source.id,
+                  minWidth: 1280,
+                  maxWidth: 1280,
+                  minHeight: 720,
+                  maxHeight: 720
+                }
               }
-            }
-          });
-          handleStream(stream);
-        } catch (e) {
-          handleError(e);
+            });
+            handleStream(stream);
+          } catch (e) {
+            handleError(e);
+          }
+          return;
         }
-        return;
       }
-    }
-  });
+    });
+}
 
 function handleStream(stream) {
   const video = document.querySelector("video");
   video.srcObject = stream;
-  video.onloadedmetadata = e => video.play();
+  video.onloadedmetadata = () => {
+    video.play();
+    // Create canvas
+    let canvas = document.createElement("canvas");
+    canvas.height = video.videoHeight;
+    canvas.width = video.videoWidth;
+
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    let img = new Image();
+    img.src = canvas.toDataURL();
+    console.log(img);
+    document.getElementById("img").setAttribute("src", img.src);
+
+    // let li = document.createElement("li");
+    // li.appendChild(img);
+    // document.getElementById("olFrames").appendChild(li);
+  };
+  console.log(stream);
 }
 
 function handleError(e) {
