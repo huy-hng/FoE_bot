e423dd67e1dcbab817e84a901944dc311bf1b23f
Huy Hoang <huyhoang0496@gmail.com>
2019-06-24 14:45:20 +0200

    remove keyboard shortcuts

--- electron/main.js
+++ electron/main.js
@@ -9,9 +9,6 @@ require("electron-reload")(__dirname, {
   argv: []
 });
 
-const { Menu, MenuItem } = require('electron');
-
-
 
 function createWindow() {
   let win = new BrowserWindow({
@@ -19,16 +16,8 @@ function createWindow() {
     height: 1080,
     webPreferences: { webviewTag: true, nodeIntegration: true }
   });
-  // win.removeMenu();
+  win.removeMenu();
   win.maximize();
-
-  const menu = new Menu();
-
-  menu.append(new MenuItem({
-    label: 'Pause',
-    accelerator: 'p',
-    click: () => { console.log('time to print stuff') }
-  }))
   
   win.loadURL(
     url.format({
