2a0a78e86053f2f9506709ddc18065e38f45a7a4
Huy Hoang <huyhoang0496@gmail.com>
2019-08-15 18:25:10 +0200

    implemented file exist check

--- electron/src/functions/process_data.ts
+++ electron/src/functions/process_data.ts
@@ -8,6 +8,7 @@ export default class Data {
   private file_location: string;
   constructor(private file_name: string) {
     this.file_location = `./data/${file_name}.json`
+
   }
 
   async set_new_data(new_data: any) {
@@ -31,13 +32,28 @@ export default class Data {
 
   async get_data() {
     const logger = logging.get_logger('read_data', 'INFO', true)
-    let data: string = fs.readFileSync(this.file_location, 'utf8')
+
+    let data: string
+    if (fs.existsSync(this.file_location)) {
+      data = fs.readFileSync(this.file_location, 'utf8')
+    } else {
+      data = this.create_file()
+    }
 
     logger.debug('data:', data);
     return JSON.parse(data);
   }
+
+  create_file() {
+    let default_settings: string
+    if (this.file_name == 'app') default_settings = JSON.stringify(app_settings)
+    if (this.file_name == 'helper') default_settings = JSON.stringify(helper_empty_params)
+
+    this.set_data(default_settings)
+    return default_settings
+  }
   
-  async set_data(data) {
+  async set_data(data: any) {
     const logger = logging.get_logger('write_data', 'INFO', true)
     fs.writeFile(this.file_location, JSON.stringify(data, null, 2), err => {
       if (err) throw err;
@@ -45,4 +61,21 @@ export default class Data {
     });
   }
 
+}
+
+
+const app_settings = {
+  "auto_login": false,
+  "auto_login_server": "",
+
+  "friends help": true,
+  "friends tavern": true,
+  "guild help": true,
+  "neighbors help": false
+}
+
+const helper_empty_params = {
+  last_checked: Math.floor(Date.now() / 1000),
+  potential_helpers: {},
+  helpers: {}
 }
